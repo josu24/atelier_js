d50ee3fb522d19a1bc9b6bf9659a8a08042b885b
Joel Sundqvist <joelsundqvist@live.se>
2019-02-12 21:27:14 +0100

    scrollmagic and lines

--- js/scrollDesc.js
+++ js/scrollDesc.js
@@ -6,24 +6,10 @@ $(function() {
 
   // get all slides
   var slides = document.querySelectorAll("div.frame");
+  var line = document.querySelector("span.descLine");
   var desc1 = slides[0];
   var desc2 = slides[1];
-  //var scenes = [];
-  //console.log(scenes.length);
-  // create scene for every slide
-  /* for (var i = 0; i < slides.length; i++) {
-    scenes[i] = new ScrollMagic.Scene({
-      triggerElement: slides[i],
-      offset: 250
-    })
-      .setPin(slides[i])
-
-      .setClassToggle("div.frame", "activeFrame")
-      .on("enter", function() {
-        slides[i].getElementsByClassName.style.background = "grey";
-      })
-      .addTo(controller);
-  } */
+  var desc3 = slides[2];
 
   scene1 = new ScrollMagic.Scene({
     triggerElement: desc1,
@@ -33,25 +19,37 @@ $(function() {
     triggerElement: desc2,
     offset: 250
   });
+  scene3 = new ScrollMagic.Scene({
+    triggerElement: desc3,
+    offset: 250,
+    duration: 3000
+  });
+  lineScene = new ScrollMagic.Scene({
+    triggerElement: line,
+    triggerHook: "onLeave",
+    offset: -300,
+    duration: "100%"
+  });
   scene1.setPin(desc1);
   scene1.addTo(controller);
   scene2.setPin(desc2);
   scene2.addTo(controller);
-  scene1.on("enter", function() {
-    desc1.style.background = "black";
-  });
-  scene1.on("leave", function() {
-    desc1.style.background = "none";
-  });
+  scene3.setPin(desc3);
+  scene3.addTo(controller);
+  lineScene.setTween(line, 2, { y: "50%", ease: Linear.ease });
+  lineScene.addTo(controller);
+  lineScene.addIndicators();
 
   function updateDuration(e) {
     width = $(window.innerWidth);
     //console.log(width[0]);
     if (width[0] < 500) {
       durationValue = window.innerWidth / 2.4;
+      protoDuration = window.innerWidth * 3.5;
       //console.log("less");
     } else {
       durationValue = window.innerWidth / 2.1;
+      protoDuration = window.innerWidth * 2.62;
       //console.log("more");
     }
 
@@ -60,7 +58,31 @@ $(function() {
     } */
     scene1.duration(durationValue);
     scene2.duration(durationValue);
+    scene3.duration(protoDuration);
   }
   updateDuration();
   $(window).on("resize", updateDuration);
+
+  function init() {
+    elems = document.querySelectorAll("div.frame");
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+
+  function addEventHandlers() {
+    window.addEventListener("scroll", checkPosition);
+    window.addEventListener("resize", init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          "hidden",
+          "fade-in-element"
+        );
+      }
+    }
+  }
 });
--- js/scrollTitle.js
+++ js/scrollTitle.js
@@ -1,27 +1,46 @@
 $(function() {
-  // wait for document ready
-  // init
+  var slides = document.querySelectorAll("span");
+  var cont = document.querySelector("div.content__main");
+  var line = document.querySelector("span.avanLineAtelier");
+
   var controller = new ScrollMagic.Controller();
 
-  // get all slides
-  var slides = document.querySelectorAll("span");
-  console.log(slides.length);
-
-  //need plugin GSAP
-
-  var slideAnimation = new TimelineMax().fromTo(
-    slides[2],
-    1,
-    { x: "-100%" },
-    { x: "0%", ease: Linear.easeNone }
-  ); // in from left
-
-  // create scene for every slide
-  scene = new ScrollMagic.Scene({
-    triggerElement: "content__main_a",
-    triggerHook: "onLeave"
-  })
-    .setPin("content__main_a")
-    .setTween(slideAnimation)
-    .addTo(controller);
+  avan = new ScrollMagic.Scene({
+    triggerElement: slides[2],
+    triggerHook: "onLeave",
+    offset: -220,
+    duration: "20%"
+  });
+  handmadeIn = new ScrollMagic.Scene({
+    triggerElement: slides[0],
+    triggerHook: "onLeave",
+    duration: "35%"
+  });
+  container = new ScrollMagic.Scene({
+    triggerElement: cont,
+    triggerHook: "onLeave",
+    duration: "70%",
+    offset: 218
+  });
+  lineScene = new ScrollMagic.Scene({
+    triggerElement: line,
+    triggerHook: "onLeave",
+    duration: "100%",
+    offset: -500
+  });
+  avan.setTween(slides[2], 0.3, { x: "30%", ease: Linear.ease });
+  avan.addIndicators();
+  avan.addTo(controller);
+  handmadeIn.setPin(slides[0]);
+  handmadeIn.addIndicators();
+  handmadeIn.addTo(controller);
+  container.addTo(controller);
+  container.addIndicators();
+  lineScene.setTween(line, 0.5, { y: "1100%", ease: Linear.ease });
+  lineScene.addTo(controller);
+  lineScene.addIndicators();
+  handmadeIn.on("end", function(event) {
+    container.setPin(slides[2]);
+    container.setTween(slides[2], 0.5, { x: "-1%", ease: Linear.ease });
+  });
 });
